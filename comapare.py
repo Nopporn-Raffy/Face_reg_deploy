04997d86704690f19cff63a145c45652c8f4c090
Nopporn-Raffy <94434737+Nopporn-Raffy@users.noreply.github.com>
2021-12-24 11:45:37 +0700

    Update comapare.py

--- comapare.py
+++ comapare.py
@@ -4,22 +4,15 @@ import pickle
 import matplotlib.image as plt
 import numpy as np
 
-list_encode = []
-# list_com = []
-match_face = False
-percentage_com = 0
-# webcam = cv2.VideoCapture(0)
-# width = int(webcam.get(3))
-# height = int(webcam.get(4))
-
-def load_data():
-    file = open("data.pkl", "rb")
-    encoded = pickle.load(file)
-    file.close()
-    return encoded
-
 def main(im) :
     try :
+        list_encode = []
+        # list_com = []
+        match_face = False
+        percentage_com = 0
+        # webcam = cv2.VideoCapture(0)
+        # width = int(webcam.get(3))
+        # height = int(webcam.get(4))
         print("Reading image")
         img = plt.imread(im)
         face_locations = fr.face_locations(img)
@@ -32,6 +25,7 @@ def main(im) :
             # result = fr.compare_faces([list_encode[0][0]], list_encode[0][1])
             percentage = fr.face_distance([list_encode[0]], list_encode[1])
             percentage = (1-percentage)*100
+            percentage = percentage - percentage%0.01
             result = fr.compare_faces([list_encode[0]], list_encode[1])
             return result, percentage
         except Exception as e:
